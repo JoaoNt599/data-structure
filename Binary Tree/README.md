70a739c9260ed25f76ea66de932f97d2a1644785
João Vieira <netojoao599@gmail.com>
2023-12-01 09:46:56 -0300

    Binary Tree

---
+++ Binary Tree/README.md
@@ -0,0 +1,27 @@
+# Binary Tree
+
+
+## A binary tree is a hierarchical data structure composed of nodes, where each node has at most two children, typically referred to as the "left child" and "right child." Each node contains a value or key, and the relationship between nodes reflects a specific order.
+
+## Key terms associated with binary trees:
+
+### Parent Node: The node that has children in a tree.
+
+### Child Node: Each node can have up to two children, referred to as the "left child" and "right child."
+
+### Leaf Node: A node that has no children.
+
+### Root: The top node in the tree hierarchy, which has no parent node.
+
+### Subtree: A tree formed by a node and all its descendants.
+
+### Tree Height: The length of the longest path from the root to a leaf.
+
+### Node Level: The distance between the node and the root, with the root at level 0.
+
+### Binary Search Tree (BST): A specific type of binary tree where, for each node:
+###     The values of nodes in the left subtree are less than the value of the node.
+###     The values of nodes in the right subtree are greater than the value of the node.
+###     This property facilitates efficient search, insertion, and deletion operations.
+
+## The structure of a binary tree facilitates quick search in ordered data and provides a foundation for many algorithms and data structures. Binary trees are widely used in computer science to represent hierarchical data and perform efficient operations on ordered datasets.
---
+++ Binary Tree/binarytree.py
@@ -0,0 +1,114 @@
+class BinaryTree:
+
+    def __init__(self, value):
+        self.key = value
+        self.left_child = None
+        self.right_child = None
+
+
+    def insert_left(self, value):
+        if self.left_child == None:
+            self.left_child = BinaryTree(value)
+        else:
+            bin_tree = BinaryTree(value)
+            bin_tree.left_child = self.left_child
+            self.left_child = bin_tree
+    
+
+    def insert_right(self, value):
+        if self.right_child == None:
+            self.right_child = BinaryTree(value)
+        else:
+            bin_tree = BinaryTree(value)
+            bin_tree.right_child = self.right_child
+            self.right_child = bin_tree
+    
+
+    def breadth_first_search(self, n):
+        current = [self]
+        next = []
+        while current:
+            for node in current:
+                if node.key == n:
+                    return True
+                if node.left_child:
+                    next.append(node.left_child)
+                if node.right_child:
+                    next.append(node.right_child)
+            current = next
+            next = []
+        return False
+
+
+    def preorder(self):
+        if self:
+            print(self.key)
+            if self.left_child:
+                self.left_child.preorder()
+            if self.right_child:
+                self.right_child.preorder()
+    
+
+    def postorder(self):
+        if self:
+            if self.left_child:
+                self.left_child.postorder()
+            if self.right_child:
+                self.right_child.postorder()
+            print(self.key)
+    
+
+    def inorder(self):
+        if self:
+            if self.left_child:
+                self.left_child.inorder()
+            print(self.key)
+            if self.right_child:
+                self.right_child.inorder()
+      
+
+    def invert(self):
+        current = [self]
+        next = []
+        while current:
+            for node in current:
+                if node.left_child:
+                    next.append(node.left_child)
+                if node.right_child:
+                    next.append(node.right_child)
+                tmp = node.left_child
+                node.left_child = node.right_child
+                node.right_child = node.left_child
+            current = next
+            next = []
+
+
+
+if __name__ == "__main__":
+    tree = BinaryTree(1)
+    tree.insert_left(2)
+    tree.insert_right(3)
+    tree.insert_left(4)
+    tree.left_child.insert_right(6)
+    tree.insert_right(5)
+
+
+    print("Pre-order:")
+    tree.preorder()
+
+
+    print("Post-order:")
+    tree.postorder()
+
+
+    print("In-order:")
+    tree.inorder()
+
+
+    result = tree.breadth_first_search(5)
+    if result:
+        print("Value 5 found in the tree.")
+    else:
+        print("Value 5 not found in tree.")
+
+    
